25c6422d27ed54f8bcc512a34343cb80b0788eaa
Magikro <majikrow@gmail.com>
2022-04-11 20:01:39 -0400

    app logo

--- UFree/App.js
+++ UFree/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   console.log("App executed");
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Image style ={styles.logo} source={require("./assets/ufreeFrog.png")}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +17,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 275,
+    height: 275,
+    resizeMode: 'contain',
   },
 });
